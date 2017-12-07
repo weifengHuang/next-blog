919da68ae9e3dce1bf696719ce6bc17117effd46
weifengHuang <463815567@qq.com>
2017-12-07 19:29:00 +0800

    feat: add HMR support

--- next.config.js
+++ next.config.js
@@ -14,5 +14,13 @@ module.exports = {
       }
     )
     return config
-  }
+  },
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
+  },  
 }
