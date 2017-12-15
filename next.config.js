f905a76b7d6bde602f611c3019900c1912dda694
weifengHuang <463815567@qq.com>
2017-12-15 19:27:11 +0800

    feat: 增加config目录

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
-const glob = require('glob')
-
+// function resolve (dir) {
+//   return path.join(__dirname, '..')
+// }
+// console.log('resolove', path.resolve(__dirname))
 module.exports = {
   webpack: (config, { dev }) => {
     config.module.rules.push(
@@ -8,19 +10,25 @@ module.exports = {
         test: /\.md$/,
         use: [
           {
-            loader: "raw-loader"
+            loader: 'raw-loader'
           }
         ]
       }
     )
+    config.resolve = {
+      extensions: ['.js', '.json'],
+      alias: {
+        '@': path.resolve(__dirname)
+      }
+    }
     return config
   },
   webpackDevMiddleware: (config) => {
     // Solve compiling problem via vagrant
     config.watchOptions = {
       poll: 1000,   // Check for changes every second
-      aggregateTimeout: 300,   // delay before rebuilding
-    };
-    return config;
-  },  
+      aggregateTimeout: 300   // delay before rebuilding
+    }
+    return config
+  }
 }
