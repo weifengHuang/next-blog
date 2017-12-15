2a730bc9cdbfbf2c6b83a08f16d7b261fb328489
weifengHuang <463815567@qq.com>
2017-12-15 11:47:28 +0800

    feat:增加env配置

---
+++ config/index.js
@@ -0,0 +1,10 @@
+const config = require('./env.json')[process.env.NODE_ENV || 'development']
+const apiUrl = config['apiUrl']
+const domainAdress = config['domainAdress']
+const imUrl = config['imUrl']
+console.log('apiUrl', apiUrl, imUrl)
+module.exports = {
+  config,
+  apiUrl,
+  domainAdress
+}
