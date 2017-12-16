8bc46b8723ce24729037e7186cf324309c6fc00d
DESKTOP-SRN6JR7\hwf <463815567@qq.com>
2017-12-16 10:46:00 +0800

    fix: im的node_redis改为ioredis
    更方便使用es6

--- im/redis/index.js
+++ im/redis/index.js
@@ -1,22 +1,14 @@
-var redis = require('redis')
-const bluebird = require('bluebird')
-bluebird.promisifyAll(redis.RedisClient.prototype)
-bluebird.promisifyAll(redis.Multi.prototype)
-const client = redis.createClient()
 
-client.on('error', function (err) {
-  console.log('Error ' + err)
-})
+var Redis = require('ioredis');
+var redis = new Redis();
 
-let test = async () => {
-  await client.setAsync('string key', 'string val')
-  let data = await client.getAsync('string key')
-  console.log('data', data)
-  client.hsetAsync('user', 'name', 11)
-  client.saddAsync('userList', JSON.stringify({a:1, b:2}))
-  let user = await client.hgetAsync('user', 'name')
-  console.log('user', user)
-  let userList = await client.smembersAsync('userList')
-  console.log('userList', userList)
-}
-test()
+// let test = async () => {
+//   redis.hset('user', 'name', 11)
+//   let data = await redis.hget('user', 'name')
+//   console.log('dataname', data)
+//   redis.hset('user', 'year', 11)
+//   let year = await redis.hget('user', 'year')
+//   console.log('year', year)
+// }
+// test()
+exports = redis
