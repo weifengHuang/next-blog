d8f81630f815d924d56884eebc7867d6b4721c4c
weifengHuang <463815567@qq.com>
2018-01-18 11:40:32 +0800

    fix: code规范

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const next = require('next')
 const app = express()
-const router = express.Router()
 const bodyParser = require('body-parser')
 const server = require('http').Server(app)
-var io = require('socket.io')(server)
-
+const io = require('socket.io')(server)
 const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const nextHandle = nextApp.getRequestHandler()
@@ -35,12 +33,13 @@ io.on('connection', (socket) => {
       name: name,
       socketId: socket.id
     }
+    // 在线列表
     loginUserList.push(user)
     fn(user)
     socket.broadcast.emit('broadcast', `广播消息${user.name}上线了`)
     io.emit('getLoginList', loginUserList)
   })
-  socket.on('disconnect', (user) => {
+  socket.on('disconnect', () => {
     console.log('用户下线了')
     loginUserList = loginUserList.filter(e => {
       return e.socketId !== socket.id
@@ -50,36 +49,36 @@ io.on('connection', (socket) => {
 })
 
 nextApp.prepare()
-.then(() => {
+  .then(() => {
   // api server
-  app.use(bodyParser.urlencoded({ extended: false }))
-// Parse application/json
-  app.use(bodyParser.json())
-  app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', '*')
-		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-		next()
-	})
-  app.get('/api/login', (req, res) => {
-    return res.json({code: 0})
+    app.use(bodyParser.urlencoded({ extended: false }))
+    // Parse application/json
+    app.use(bodyParser.json())
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+      next()
+    })
+    app.get('/api/login', (req, res) => {
+      return res.json({code: 0})
+    })
+    // mongodb connection
+    require('./model')
+    // Next request
+    app.get('/p/:id', (req, res) => {
+      const actualPage = '/post'
+      const queryParams = { id: req.params.id }
+      app.render(req, res, actualPage, queryParams)
+    })
+    app.get('*', (req, res) => {
+      return nextHandle(req, res)
+    })
+    server.listen(port, (err) => {
+      if (err) throw err
+      console.log(`> Ready on ${apiUrl}`)
+    })
   })
-  // mongodb connection
-  require('./model')
-  // Next request
-  app.get('/p/:id', (req, res) => {
-    const actualPage = '/post'
-    const queryParams = { id: req.params.id }
-    app.render(req, res, actualPage, queryParams)
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
   })
-  app.get('*', (req, res) => {
-    return nextHandle(req, res)
-  })
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on ${apiUrl}` )
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
