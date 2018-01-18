805bd9d608b87d4a46fb984357919a112f7c0db7
weifengHuang <463815567@qq.com>
2018-01-18 11:40:08 +0800

    feat： 增加sockethandle
    登陆以后写入到mongodb

--- socket/index.js
+++ socket/index.js
@@ -1,14 +1,34 @@
-var Redis = require('ioredis')
-var redis = new Redis()
+// const Redis = require('ioredis')
+// const redis = new Redis()
+const User = require('../model/user.js')
 const SocketHandle = class SocketHandle {
   constructor () {
   }
-  static saveUserSocketId (socketId, userName) {
-    redis.hset('userList', userName, socketId)
-    console.log('执行结束')
+  static async saveUserSocketId (name, socketId) {
+    try {
+      let createdUser = await User.create({
+        name: name,
+        socketId: socketId
+      })
+      return createdUser
+    } catch (e) {
+      console.error('e', e)
+      return false
+    }
+    // redis.hset('userList', userName, socketId)
   }
-  static async getUserSocketId (socketId, userName) {
-    await redis.hget('userList', userName, userName)
+  static async getUserSocketId (name) {
+    try {
+      let user = await User.findOne({name: name})
+      if (user) {
+        return user.socketId
+      } else {
+        return null
+      }
+    } catch (e) {
+      return false
+    }
+    // await redis.hget('userList', userName, userName)
   }
 }
 module.exports = SocketHandle
