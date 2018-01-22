a60ef3ba3c4f6d3de954d7022ba531dd45d4f481
weifengHuang <463815567@qq.com>
2018-01-22 19:36:08 +0800

    fix: 修改handle

--- socket/index.js
+++ socket/index.js
@@ -4,11 +4,18 @@ const User = require('../model/user.js')
 const SocketHandle = class SocketHandle {
   constructor () {
   }
-  static async saveUserSocketId (name, socketId) {
+  static async saveUserSocketId (params) {
     try {
+      let {name, socketId, password} = params
+      let encrypted = User.saltPassword(password)
+      if (!name || !password) {
+        return {code: 1, message: 'name or password not null'}
+      }
       let createdUser = await User.create({
         name: name,
-        socketId: socketId
+        socketId: socketId,
+        onlineStatus: 0,
+        password: encrypted
       })
       return createdUser
     } catch (e) {
