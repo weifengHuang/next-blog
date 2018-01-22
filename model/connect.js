d34e467ede4952ccc23dae926434b14113966199
weifengHuang <463815567@qq.com>
2018-01-22 19:35:51 +0800

    feat:  增加connect

---
+++ model/connect.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const chalk = require('chalk')
+const {mongodb} = require('../config')
+mongoose.connect(mongodb, {useMongoClient: true})
+mongoose.Promise = global.Promise
+
+const db = mongoose.connection
+
+db.once('open', () => {
+  console.log(chalk.green('连接数据库成功'))
+})
+
+db.on('error', function (error) {
+  console.error(
+    chalk.red('Error in MongoDb connection: ' + error)
+  )
+  mongoose.disconnect()
+})
+
+db.on('close', function () {
+  console.log(
+    chalk.red('数据库断开，重新连接数据库')
+  )
+  mongoose.connect(mongodb, {useMongoClient: true, server: {auto_reconnect: true}})
+})
+
+exports = db
